2d5c255a3170faa8bf61886ec030b3f3bf1fb640
shihyao001 <shihyao001@ibears.io>
2024-07-07 17:50:17 +0800

    rm

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ const apiRoute = require('./routes/index.v1');
 
 const app = express();
 
-// View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
